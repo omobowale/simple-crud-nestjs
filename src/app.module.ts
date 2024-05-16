2140d15269cc7204a25e33f4aefbc80d414e4324
Omobowale <omobowale92@outlook.com>
2024-05-16 12:13:41 +0100

    set up broilerplate for project using nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [TasksModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
